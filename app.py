91882cf1ec5c535467e58eb5776e5c8116aaac92
LIN WAN-HAW <10456003@ntub.edu.tw>
2019-12-26 21:12:11 +0800

    myApp

--- app.py
+++ app.py
@@ -1,47 +1,60 @@
 from flask import Flask, request, abort
-import flask
-flask.path.append("module/flask")
+from urllib.request import urlopen
+
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError,LineBotApiError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+
+################################
+
+from linebot.models import *
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('ZvAXCuzk2y7F6b5mmRHMGEale/T6gIyz5XsvBYUsxDID11m9LDCxnp9+x5f6sLkP+Zlh5D+v3LVaymuIt1Mh8PSd7+2RYM+2NxJyQguGY7o5Xtrxx+zaYs2BDhtGE1vv7RtX7kP1xNzETMgyx5T3OgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('318d67366cb286f75b9894da10dc36af')
 
-
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    print(event)
+    text=event.message.text
 
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
 
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
+    else:
+        reply_text = text
+#如果非以上的選項，就會學你說話
+
+    message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

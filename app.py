7f2a124b10ccd6495f0707a6e5e3313c70303fed
LIN WAN-HAW <10456003@ntub.edu.tw>
2019-12-27 19:44:26 +0800

    myApp

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi(
-    '4O8vNwJN4S3g6m6UtTe6T9hhUW0rXnIH4aK52CMEJFL53XyzQQkZZg5o90w8y1p9+Zlh5D+v3LVaymuIt1Mh8PSd7+2RYM+2NxJyQguGY7qE/GI0Ttwbzk5HmMmBS2RSzjzSIzVPgE7WyNQCDLPpywdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('4O8vNwJN4S3g6m6UtTe6T9hhUW0rXnIH4aK52CMEJFL53XyzQQkZZg5o90w8y1p9+Zlh5D+v3LVaymuIt1Mh8PSd7+2RYM+2NxJyQguGY7qE/GI0Ttwbzk5HmMmBS2RSzjzSIzVPgE7WyNQCDLPpywdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('318d67366cb286f75b9894da10dc36af')
 
@@ -37,151 +36,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    json = {
-        "type": "flex",
-        "altText": "Flex Message",
-        "contents": {
-            "type": "bubble",
-            "hero": {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_3_movie.png",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "action": {
-                    "type": "uri",
-                    "label": "Action",
-                    "uri": "https://linecorp.com/"
-                }
-            },
-            "body": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "md",
-                "contents": [
-                    {
-                        "type": "text",
-                        "text": "電影名稱",
-                        "size": "xl",
-                        "gravity": "center",
-                        "weight": "bold",
-                        "wrap": true
-                    },
-                    {
-                        "type": "box",
-                        "layout": "baseline",
-                        "margin": "md",
-                        "contents": [
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "size": "sm"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "size": "sm"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "size": "sm"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "size": "sm"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
-                                "size": "sm"
-                            },
-                            {
-                                "type": "text",
-                                "text": "4.0",
-                                "flex": 0,
-                                "margin": "md",
-                                "size": "sm",
-                                "color": "#999999"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "vertical",
-                        "spacing": "sm",
-                        "margin": "lg",
-                        "contents": [
-                            {
-                                "type": "box",
-                                "layout": "baseline",
-                                "spacing": "sm",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "text": "上映日期",
-                                        "flex": 2,
-                                        "size": "sm",
-                                        "color": "#AAAAAA"
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "Monday 25, 9:00PM",
-                                        "flex": 4,
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "wrap": true
-                                    }
-                                ]
-                            },
-                            {
-                                "type": "box",
-                                "layout": "baseline",
-                                "spacing": "sm",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "text": "電影簡介",
-                                        "flex": 2,
-                                        "size": "sm",
-                                        "color": "#AAAAAA"
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "C Row, 18 Seat",
-                                        "flex": 4,
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "wrap": true
-                                    }
-                                ]
-                            },
-                            {
-                                "type": "box",
-                                "layout": "vertical",
-                                "margin": "xxl",
-                                "contents": [
-                                    {
-                                        "type": "spacer"
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "資料來源：Yahoo電影",
-                                        "margin": "xxl",
-                                        "size": "xs",
-                                        "color": "#AAAAAA",
-                                        "wrap": true
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-    message = TemplateSendMessage(json)
+    message = """{
+      type: 'text',
+      text: 'hihihihi'
+    } """
     line_bot_api.reply_message(event.reply_token, message)
 
 

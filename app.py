05e365eb01a907d44f5b98e72d981be3aa908338
LIN WAN-HAW <10456003@ntub.edu.tw>
2020-01-03 20:49:30 +0800

    myApp

--- app.py
+++ app.py
@@ -71,30 +71,54 @@ def handle_message(event):
             for url in rating_url:
                 urllist.append(url["href"])
             
-            buttons_template_message = TemplateSendMessage(
-                alt_text='Buttons template',
-                template=ButtonsTemplate(
-                    thumbnail_image_url=imglist[0],
-                    title=rating_name[0],
-                    text='上映日期：...',
-                    actions=[
-                        PostbackAction(
-                            label='postback',
-                            display_text='postback text',
-                            data='action=buy&itemid=1'
+            carousel_template_message = TemplateSendMessage(
+                alt_text='Carousel template',
+                template=CarouselTemplate(
+                    columns=[
+                        CarouselColumn(
+                            thumbnail_image_url='https://example.com/item1.jpg',
+                            title='this is menu1',
+                            text='description1',
+                            actions=[
+                                PostbackAction(
+                                    label='postback1',
+                                    display_text='postback text1',
+                                    data='action=buy&itemid=1'
+                                ),
+                                MessageAction(
+                                    label='message1',
+                                    text='message text1'
+                                ),
+                                URIAction(
+                                    label='uri1',
+                                    uri='http://example.com/1'
+                                )
+                            ]
                         ),
-                        MessageAction(
-                            label='網址',
-                            text=urllist[0]
-                        ),
-                        URIAction(
-                            label='uri',
-                            uri='http://example.com/'
+                        CarouselColumn(
+                            thumbnail_image_url='https://example.com/item2.jpg',
+                            title='this is menu2',
+                            text='description2',
+                            actions=[
+                                PostbackAction(
+                                    label='postback2',
+                                    display_text='postback text2',
+                                    data='action=buy&itemid=2'
+                                ),
+                                MessageAction(
+                                    label='message2',
+                                    text='message text2'
+                                ),
+                                URIAction(
+                                    label='uri2',
+                                    uri='http://example.com/2'
+                                )
+                            ]
                         )
                     ]
                 )
             )
-            line_bot_api.reply_message(event.reply_token, buttons_template_message)
+            line_bot_api.reply_message(event.reply_token, carousel_template_message)
         except Exception as e:
             print(str(e))
     else:

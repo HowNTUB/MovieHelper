0ce26386fc04b30e1a1861d111588e20ceea97d3
LIN WAN-HAW <10456003@ntub.edu.tw>
2020-01-03 22:43:09 +0800

    myApp

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
     body = flask.request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    print(body.data)
+    print(body)
     # handle webhook body
     try:
         handler.handle(body, signature)

ebcb047396841a50a0bdf1f663b5522bb875f137
LIN WAN-HAW <10456003@ntub.edu.tw>
2020-01-03 19:23:21 +0800

    myApp

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
-    body = flask.request.get_data(as_text=True)
+    body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:

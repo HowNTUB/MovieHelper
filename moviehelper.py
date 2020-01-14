d1e7bf6e79b3fd64accb188e276f3ae2940b153f
LIN WAN-HAW <10456003@ntub.edu.tw>
2020-01-14 21:34:39 +0800

    myApp

--- moviehelper.py
+++ moviehelper.py
@@ -3,12 +3,14 @@ from urllib import parse
 from bs4 import BeautifulSoup
 from linebot.models import *
 
+
 def use_moviename_serch_movielist(movieName):
     try:
         # 中文轉URL格式編碼
         urlname = parse.quote(movieName)
         # 電影清單URL
-        movieURL = 'https://movies.yahoo.com.tw/moviesearch_result.html?keyword=' + urlname + '&type=movie'
+        movieURL = 'https://movies.yahoo.com.tw/moviesearch_result.html?keyword=' + \
+            urlname + '&type=movie'
         headers = {}
         headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
         req = request.Request(movieURL, headers=headers)
@@ -26,9 +28,10 @@ def use_moviename_serch_movielist(movieName):
         movieNameCN = [i.text for i in soup.select(".release_movie_name > a")]
         movieNameEN = [i.text for i in soup.select(".en a")]
         movieExpectation = [i.text for i in soup.select("#content_l dt span")]
-        movieSatisfactoryDegree=[]
+        movieSatisfactoryDegree = []
         for info in movieInfo:
-            movieSatisfactoryDegree.append('未上映') if info.find("滿意度")==-1 else movieSatisfactoryDegree.append(info[info.find("滿意度")+5:info.find("滿意度")+8])
+            movieSatisfactoryDegree.append('未上映') if info.find(
+                "滿意度") == -1 else movieSatisfactoryDegree.append(info[info.find("滿意度")+5:info.find("滿意度")+8])
         movieImg = [i for i in soup.select(".release_foto img")]
         moviePoster = []
         for img in movieImg:
@@ -197,4 +200,207 @@ def use_moviename_serch_movielist(movieName):
         print(str(e))
 
 
-#def use_movieurl_get_movieinfo(url):
+def use_movieurl_get_movieinfo(url):
+    try:
+        url = 'https://movies.yahoo.com.tw/movieinfo_main/STAR-WARS-%E5%A4%A9%E8%A1%8C%E8%80%85%E7%9A%84%E5%B4%9B%E8%B5%B7-star-wars-the-rise-of-skywalker-10225'
+        headers = {}
+        headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+        req = request.Request(url, headers=headers)
+        resp = request.urlopen(req)
+        respData = str(resp.read().decode('utf-8'))  # 將所得的資料解碼
+        soup = BeautifulSoup(respData)
+
+        # --------------------info
+        movieNameCN = soup.select_one("h1").text
+        movieNameEN = soup.select_one(".movie_intro_info_r h3").text
+        movieTagStr = [i.text for i in soup.select(".level_name .gabtn")]
+        movieTag = []
+        for tag in movieTagStr:
+            movieTag.append(tag.split()[0])
+        movieReleaseTimeStr = [
+            i.text for i in soup.select(".level_name_box+ span")]
+        for date in movieReleaseTimeStr:
+            movieReleaseTime = date[5:]
+        movieRuntime = (soup.select_one("span:nth-child(6)").text)[5:]
+        movieProCo = (soup.select("span:nth-child(7)")[1].text)[5:]
+        movieIMDb = (soup.select_one("span:nth-child(8)").text)[7:]
+        movieExpectation = (
+            (soup.select(".evaluate_inner")[0].text).split())[2]
+        movieSatisfactoryDegree = (
+            (soup.select(".evaluate_inner")[1].text).split())[3]
+
+        # 彈性訊息
+        movieTagContent = []
+        for tag in movieTag:
+            movieTagContent.append({
+                "type": "text",
+                "text": tag,
+                "flex": 0,
+                "weight": "bold",
+                "color": "#000C3B"
+            })
+        flex_message = FlexSendMessage(
+            alt_text='movielist',
+            contents={
+                "type": "bubble",
+                "direction": "ltr",
+                "header": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": "電影資訊(Infomation)",
+                            "size": "xl",
+                            "align": "start",
+                            "weight": "bold"
+                        }
+                    ]
+                },
+                "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": movieNameCN,
+                            "align": "center",
+                            "weight": "bold"
+                        },
+                        {
+                            "type": "text",
+                            "text": movieNameEN,
+                            "size": "xs",
+                            "align": "center"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "margin": "sm",
+                            "contents": movieTagContent
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "margin": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "上映日期",
+                                    "weight": "bold"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": movieReleaseTime
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "margin": "xs",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "片　　長",
+                                    "weight": "bold"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": movieRuntime
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "margin": "xs",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "發行公司",
+                                    "weight": "bold"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": movieProCo
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "margin": "xs",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "IMDb分數",
+                                    "weight": "bold",
+                                    "color": "#EAC44E"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": movieIMDb
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "margin": "xs",
+                            "contents": [
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "期待度",
+                                            "weight": "bold",
+                                            "color": "#BB21CA"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": movieExpectation
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "滿意度",
+                                            "weight": "bold",
+                                            "color": "#2133CA"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": movieSatisfactoryDegree
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        )
+        # 回復
+        return(flex_message)
+        # --------------------actor
+        actorName = [i.text for i in soup.select(".actor_inner h2")]
+        actorNameCN = []
+        actorNameEN = []
+        for name in actorName:
+            name = name.split()
+            actorNameCN.append(name[0])
+            actorNameEN.append(name[1]+' '+name[2])
+        movieImg = [i for i in soup.select("._slickcontent .fotoinner img")]
+        moviePoster = []
+        for img in movieImg:
+            moviePoster.append(img["src"])
+        # --------------------
+    except Exception as e:
+        print(str(e))

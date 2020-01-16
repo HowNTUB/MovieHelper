bde74952af695c105b09b7e3bfba9cd5afd17697
LIN WAN-HAW <10456003@ntub.edu.tw>
2020-01-16 18:53:58 +0800

    myApp

--- moviehelper.py
+++ moviehelper.py
@@ -468,7 +468,7 @@ def use_movieurl_get_movieinfo(url):
             }
         )
 
-        movieIntroduction = [(soup.select_one(".gray_infobox_inner").text)]
+        movieIntroduction = [(soup.select_one(".gray_infobox_inner").text)[14:-16]]
         print(movieIntroduction)
         movieIntroduction_text_message = TextSendMessage(text=movieIntroduction)
 

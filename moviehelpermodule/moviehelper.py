4e8af66b682b59e5cee6f83f41bc07cb3358c1b2
WANHAW <10456003@ntub.edu.tw>
2020-02-08 23:10:24 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -1871,13 +1871,9 @@ def search_movie_type(typeName, url):
 
 def use_location_search_movietheater(userAddress, userLat, userLng):
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+str(userLat)+','+str(userLng)+'&radius=5000&keyword=movietheater&key=AIzaSyATyj-s1QtmrmCFQIsDhnPxS4-D929PlxM&language=zh-TW'
-    heroku_ip_request = request.urlopen(url)
-    heroku_ip_response = heroku_ip_request.read()
-    os.environ['http_proxy'] = os.environ['http://lzjsv51uedbimh:pvnf4f1yf5huunkzyecjvmihgb@proxy.quotaguard.com:9292']
-    proxy = request.ProxyHandler()
-    opener = request.build_opener(proxy)
-    in_ = opener.open(url)
-    qg_response = str(in_.read().decode('utf-8'))
+    proxies = 'http://lzjsv51uedbimh:pvnf4f1yf5huunkzyecjvmihgb@proxy.quotaguard.com:9292'
+    r=requests.get("http://icanhazip.com", proxies=proxies)
+    qg_response = str(r.read().decode('utf-8'))
     soup = BeautifulSoup(qg_response ,features="html.parser")
     jsondata = json.loads(respData)
 

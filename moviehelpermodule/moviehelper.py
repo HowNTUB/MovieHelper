8d54b2ebe5f229821333cd44af21fda2ada5f195
WANHAW <10456003@ntub.edu.tw>
2020-02-17 17:27:38 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -997,8 +997,9 @@ def use_actorURL_search_movielist(url):
     headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
     req = request.Request(url, headers=headers)
     resp = request.urlopen(req)
-    respData = str(resp.read().decode('utf-8'))
-    soup = BeautifulSoup(respData, "html.parser")
+    respData = str(resp.read().decode('utf-8'))  # 將所得的資料解碼
+    soup = BeautifulSoup(respData)
+
     # movieNameCN 中文名
     # movieNameEN 英文名
     # movieExpectation 期待值

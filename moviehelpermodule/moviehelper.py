918d375efc0120233e4436079711beb546631177
WANHAW <10456003@ntub.edu.tw>
2020-02-19 20:20:04 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2558,7 +2558,7 @@ def use_movieurl_get_movieMoment(movieID, areaNo):
 
     movietheaterHtml = [i.text.strip() for i in soup.select("#filmShowtimeBlock ul")]
     movietheaterContents = []
-    for content in movietheaterHtml:
+    for content in movietheaterHtml[:10]:
         print(content.split())
         print("="*10)
         timeContents = []

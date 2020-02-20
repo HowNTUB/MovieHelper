a764d7da56c922b47d6cc9d3791912d2435f3788
WANHAW <10456003@ntub.edu.tw>
2020-02-20 17:54:39 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2619,46 +2619,65 @@ def use_movieurl_get_movieMoment(movieID, areaNo, page):
 
     totalPage = int(len(movietheaterData)/10)
     print(totalPage)
-    nowPage = int(page)
-    print(nowPage)
-    contents = []
-    for index in range(totalPage):
-        print(index)
-        contents.append({
-            "type": "text",
-            "text": str(index+1),
-            "align": "center",
-            "action": {
-                "type": "postback",
-                "data": "電影時刻"+movieID+"/a02/,"+str(index+1)
-            }
-        })
-    # 回復
-    pagebox_flex_message = FlexSendMessage(
-        alt_text='pagebox',
-        contents={
-            "type": "bubble",
-            "direction": "ltr",
-            "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
+    if totalPage>0 :
+        nowPage = int(page)
+        print(nowPage)
+        contents = []
+        for index in range(totalPage):
+            print(index)
+            contents.append({
                 "type": "text",
-                "text": "目前第"+str(nowPage)+"頁",
-                "align": "center"
-                },
-                {
-                "type": "box",
-                "layout": "horizontal",
-                "margin": "xl",
-                "contents": contents
+                "text": str(index+1),
+                "align": "center",
+                "action": {
+                    "type": "postback",
+                    "data": "電影時刻"+movieID+"/a02/,"+str(index+1)
                 }
-            ]
+            })
+        # 回復
+        pagebox_flex_message = FlexSendMessage(
+            alt_text='pagebox',
+            contents={
+                "type": "bubble",
+                "direction": "ltr",
+                "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                    "type": "text",
+                    "text": "目前第"+str(nowPage)+"頁",
+                    "align": "center"
+                    },
+                    {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "margin": "xl",
+                    "contents": contents
+                    }
+                ]
+                }
             }
-        }
-    )
-
+        )
+    else:
+        pagebox_flex_message = FlexSendMessage(
+            alt_text='pagebox',
+            contents={
+                "type": "bubble",
+                "direction": "ltr",
+                "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                    "type": "text",
+                    "text": "僅一頁搜尋結果",
+                    "align": "center"
+                    }
+                ]
+                }
+            }
+        )
     return(movietheater_flex_message, pagebox_flex_message)
 
 

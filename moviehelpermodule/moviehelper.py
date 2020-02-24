ae880009251ade7c8619684ec54997a602244313
WANHAW <10456003@ntub.edu.tw>
2020-02-24 21:45:25 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2695,7 +2695,10 @@ def use_movieurl_get_movieMoment(movieID, inAreaID, page):
         movietheaterName = content.find("li").text
         timeContents = []
         number=['1','2','3','4','5','6','7','8','9','0']
-        print((content.select("li")[1].text).split('\n')[1])
+        print('content.select("li")')
+        print(content.select("li"))
+        print('content.select("li")[1].text')
+        print(content.select("li")[1].text)
         if content.select("li")[1].text[-1] not in number:
             timeContents.append({
                 "type": "box",
@@ -2704,7 +2707,7 @@ def use_movieurl_get_movieMoment(movieID, inAreaID, page):
                 "contents": [
                     {
                     "type": "text",
-                    "text": (content.select("li")[1].text).split('\n')[1],
+                    "text": content.select("li")[1].text,
                     "size": "lg",
                     "align": "center",
                     "weight": "bold"

60b99186e27c9e4411d1c0e9aba9d01242360656
WANHAW <10456003@ntub.edu.tw>
2020-02-24 21:15:00 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2694,7 +2694,7 @@ def use_movieurl_get_movieMoment(movieID, inAreaID, page):
     for content in movietheaterData[(int(page)-1)*10:int(page)*10]:
         movietheaterName = content.find("li").text
         timeContents = []
-        for movietype in [i.text.strip() for i in content.select("li")]:
+        for movietype in [i.text.strip() for i in content.select("li")[2]]:
             print(movietype)
             print('*'*20)
             timeContents.append({
@@ -2704,7 +2704,7 @@ def use_movieurl_get_movieMoment(movieID, inAreaID, page):
                 "contents": [
                     {
                     "type": "text",
-                    "text": movietype,
+                    "text": movietype[:3],
                     "size": "lg",
                     "align": "center"
                     },

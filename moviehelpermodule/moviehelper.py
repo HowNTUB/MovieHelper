f12bd68578be993d62ec8e4694d4ca3ec77dc672
WANHAW <10456003@ntub.edu.tw>
2020-02-24 18:46:51 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2699,6 +2699,7 @@ def use_movieurl_get_movieMoment(movieID, inAreaID, page):
             now=time.strftime("%H:%M", time.localtime(time.time()+28800))
             number = ['1','2','3','4','5','6','7','8','9','0']
             if movietime.text[-1] in number: #如果是時間(有可能是放映規格)
+                print(int(movietime.text[:2]))
                 if int(now[:2])>=int(movietime.text[:2]):
                     if int(now[3:])>int(movietime[3:]): #超過放映時間
                         timeContents.append({

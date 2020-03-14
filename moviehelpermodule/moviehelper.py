6f6d40d297e42a00903a5606da6a2292b2451c22
WANHAW <10456003@ntub.edu.tw>
2020-03-14 18:22:30 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2731,7 +2731,7 @@ def use_location_search_movietheater(userAddress, userLat, userLng):
 
     googleAPIKey = os.environ['GOOGLE_API_KEY'] # 在heroku專案中的Config Vars中設定
     gmaps = googlemaps.Client(key=googleAPIKey)
-    nearbyMovietheater = googlemaps.places.places_nearby(location=(userLat,userLng), language="zh-TW", keyword="movietheater", rankby="distance", client=gmaps)
+    nearbyMovietheater = googlemaps.places.places_nearby(location=(userLat,userLng), rankby="distance", language="zh-TW", keyword="movietheater", client=gmaps)
     print(nearbyMovietheater)
     movietheaterName = []
     movietheaterLat = []

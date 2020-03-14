7a0c29537e57b1584e5a6fdaf45be357fde40f35
WANHAW <10456003@ntub.edu.tw>
2020-03-14 20:19:51 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -2725,47 +2725,66 @@ def show_location_message():
     )
     return(location_flex_message)
 def use_location_search_movietheater(userAddress, userLat, userLng):
-    try:
-        import googlemaps
-        import os
-        os.environ['http_proxy'] = os.environ['QUOTAGUARD_URL']
+    import googlemaps
+    import os
+    os.environ['http_proxy'] = os.environ['QUOTAGUARD_URL']
 
-        googleAPIKey = os.environ['GOOGLE_API_KEY'] # 在heroku專案中的Config Vars中設定
-        gmaps = googlemaps.Client(key=googleAPIKey)
-        nearbyMovietheater = googlemaps.places.places_nearby(location=(userLat,userLng), rank_by="distance", language="zh-TW", keyword="影城", client=gmaps)
-        print(nearbyMovietheater)
-        movietheaterName = []
-        movietheaterLat = []
-        movietheaterLng = []
-        movietheaterPhotos = []
-        movietheaterRating = []
-        movietheaterAddress = []
-        movietheaterDistance = []
-        for data in nearbyMovietheater["results"]:
-            movietheaterName.append(data["name"])
-            movietheaterLat.append(data["geometry"]["location"]["lat"])
-            movietheaterLng.append(data["geometry"]["location"]["lng"])
-            distance = getDistance(userLat,userLng,data["geometry"]["location"]["lat"],data["geometry"]["location"]["lng"])
-            movietheaterDistance.append(distance)
-            if distance < 2:
-                movietheaterPhotos.append("https://i.imgur.com/5HQbSSD.png")
-            elif distance < 6:
-                movietheaterPhotos.append("https://i.imgur.com/Xfu8rQU.png")
-            elif distance < 10:
-                movietheaterPhotos.append("https://i.imgur.com/3s4OfPN.png")
-            elif distance <30:
-                movietheaterPhotos.append("https://i.imgur.com/HW88JUy.png")
-            else:
-                movietheaterPhotos.append("https://i.imgur.com/GfGsFuy.png")
-            # try: #用googleAPI去抓電影院的圖片 可是這樣很浪費流量
-            #     photoReference = data["photos"][0]['photo_reference']
-            #     movietheaterPhotos.append('https://maps.googleapis.com/maps/api/place/photo?maxheight=900&maxwidth=1200&photoreference=' +
-            #                               photoReference+'&key=AIzaSyATyj-s1QtmrmCFQIsDhnPxS4-D929PlxM')
-            # except:
-            #     movietheaterPhotos.append('https://i.imgur.com/CMAl4DQ.jpg')
-            movietheaterRating.append(data["rating"])
-            movietheaterAddress.append(data["vicinity"])
-        contents = []
+    googleAPIKey = os.environ['GOOGLE_API_KEY'] # 在heroku專案中的Config Vars中設定
+    gmaps = googlemaps.Client(key=googleAPIKey)
+    nearbyMovietheater = googlemaps.places.places_nearby(location=(userLat,userLng), rank_by="distance", language="zh-TW", keyword="影城", client=gmaps)
+    print(nearbyMovietheater)
+    movietheaterName = []
+    movietheaterLat = []
+    movietheaterLng = []
+    movietheaterPhotos = []
+    movietheaterRating = []
+    movietheaterAddress = []
+    movietheaterDistance = []
+    for data in nearbyMovietheater["results"]:
+        movietheaterName.append(data["name"])
+        movietheaterLat.append(data["geometry"]["location"]["lat"])
+        movietheaterLng.append(data["geometry"]["location"]["lng"])
+        distance = getDistance(userLat,userLng,data["geometry"]["location"]["lat"],data["geometry"]["location"]["lng"])
+        movietheaterDistance.append(distance)
+        if distance < 2:
+            movietheaterPhotos.append("https://i.imgur.com/5HQbSSD.png")
+        elif distance < 6:
+            movietheaterPhotos.append("https://i.imgur.com/Xfu8rQU.png")
+        elif distance < 10:
+            movietheaterPhotos.append("https://i.imgur.com/3s4OfPN.png")
+        elif distance <30:
+            movietheaterPhotos.append("https://i.imgur.com/HW88JUy.png")
+        else:
+            movietheaterPhotos.append("https://i.imgur.com/GfGsFuy.png")
+        # try: #用googleAPI去抓電影院的圖片 可是這樣很浪費流量
+        #     photoReference = data["photos"][0]['photo_reference']
+        #     movietheaterPhotos.append('https://maps.googleapis.com/maps/api/place/photo?maxheight=900&maxwidth=1200&photoreference=' +
+        #                               photoReference+'&key=AIzaSyATyj-s1QtmrmCFQIsDhnPxS4-D929PlxM')
+        # except:
+        #     movietheaterPhotos.append('https://i.imgur.com/CMAl4DQ.jpg')
+        movietheaterRating.append(data["rating"])
+        movietheaterAddress.append(data["vicinity"])
+    contents = []
+    if len(movietheaterName)==0:
+        movietheater_flex_message = FlexSendMessage(
+            alt_text='附近沒有電影院',
+            contents={
+                "type": "bubble",
+                "direction": "ltr",
+                "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                    "type": "text",
+                    "text": "這附近沒有電影院",
+                    "align": "center"
+                    }
+                ]
+                }
+            }
+        )
+    elif:
         for index in range(len(movietheaterName[:10])):
             contents.append({
                 "type": "bubble",
@@ -2877,27 +2896,7 @@ def use_location_search_movietheater(userAddress, userLat, userLng):
                 "contents": contents
             }
         )
-        return(movietheater_flex_message)
-    except:
-        noFind_flex_message = FlexSendMessage(
-            alt_text='附近沒有電影院',
-            contents={
-                "type": "bubble",
-                "direction": "ltr",
-                "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                    {
-                    "type": "text",
-                    "text": "這附近沒有電影院",
-                    "align": "center"
-                    }
-                ]
-                }
-            }
-        )
-        return(noFind_flex_message)
+    return(movietheater_flex_message)
 
 def use_movietheatherName_search_movie(movietheaterName, page):
 

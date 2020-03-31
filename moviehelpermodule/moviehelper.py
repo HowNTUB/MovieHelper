be01fdafb3fc9962122b9fbbbbb3aca7b8ba0a3a
WANHAW <10456003@ntub.edu.tw>
2020-03-31 20:27:35 +0800

    myApp

--- moviehelpermodule/moviehelper.py
+++ moviehelpermodule/moviehelper.py
@@ -715,7 +715,7 @@ def use_movieurl_get_movieinfo(url):
         # --------------------story
         story = soup.select_one("#story").text
         print(story)
-        print(soup.select_one(".storeinfo"))
+        print(soup.find("gray_infobox_inner"))
         story_flex_message = FlexSendMessage(
             alt_text='簡介',
             contents={
